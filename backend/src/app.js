db8a0799f04b43dbcd8e41875da3900f24ed7882
ravib31 <rkb0808@gmail.com>
2024-10-13 11:17:31 +0530

    jwt and cookies implemented

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,27 +1,34 @@
 const express = require("express");
 const connectDB = require("./config/database.js");
 const User = require("./models/user.js");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const { validateSignupData } = require("./utils/validatoin.js");
+var cookieParser = require("cookie-parser");
+var jwt = require("jsonwebtoken");
 const app = express();
 require("./config/database.js");
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post("/signup", async (req, res) => {
   try {
     validateSignupData(req);
-    const {firstName,lastName,email,password}= req.body;
-    const passwordHash= await bcrypt.hash(password,10);
+    const { firstName, lastName, email, password } = req.body;
+    const passwordHash = await bcrypt.hash(password, 10);
     // console.log(passwordHash);
-    const user = new User({ firstName,lastName,email,password:passwordHash });
+    const user = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
     await user.save();
     res.send("User added successfully");
   } catch (error) {
     res.status(500).send("Error adding user" + error.message);
   }
 });
-
 
 app.post("/login", async (req, res) => {
   try {
@@ -34,12 +41,37 @@ app.post("/login", async (req, res) => {
     if (!isMatch) {
       return res.status(401).send("Invalid credentials");
     }
+    const token = await jwt.sign({ _id: user._id }, "Bhashkar");
+    console.log(token);
+    res.cookie("token", token);
     res.send("Login successful");
   } catch (error) {
     res.status(500).send("Error logging in" + error.message);
   }
 });
 
+app.get("/profile", async (req, res) => {
+  try {
+    const cookie = req.cookies;
+    console.log(cookie);
+    const { token } = cookie;
+    if (!token) {
+      throw new Error("Please login first");
+    }
+    const decodedMessage = await jwt.verify(token, "Bhashkar");
+    console.log(decodedMessage);
+    const { _id } = decodedMessage;
+    console.log("Logged In User-" + _id);
+    const user = await User.findById(_id);
+    if(!user){
+      throw new Error("User not found");
+    }
+    console.log(user);
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Error :" + error.message);
+  }
+});
 
 app.get("/user", async (req, res) => {
   // console.log(req.body);
@@ -95,16 +127,21 @@ app.patch("/user/:userId", async (req, res) => {
 
   try {
     const ALLOWED_UPDATED = [
-      "photoUrl","about","password","gender","age","skills"
-    ]
+      "photoUrl",
+      "about",
+      "password",
+      "gender",
+      "age",
+      "skills",
+    ];
     const isUpdateAllowed = Object.keys(userData).every((key) =>
       ALLOWED_UPDATED.includes(key)
     );
-    if(!isUpdateAllowed){
-      throw new Error("Invalid update")
+    if (!isUpdateAllowed) {
+      throw new Error("Invalid update");
     }
-    if(userData.skills.length>10){
-      throw new Error("Skills cannot be more than 10")
+    if (userData.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
     }
     const user = await User.findByIdAndUpdate({ _id: userId }, userData, {
       returnDocument: "after",
